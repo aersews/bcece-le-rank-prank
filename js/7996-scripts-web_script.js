8d18734ebfff306f82030bf736b5a68e0f8c3b2a
baluga <acccccccc31@gmail.com>
2025-06-25 18:22:05 +0530

    first commit

---
+++ js/7996-scripts-web_script.js
@@ -0,0 +1,6 @@
+function openpopup(pg)
+	{
+		//alert(pg);
+		window.open(pg,"win","top=5,left=30,maximize=yes,toolbars=no,resize=yes,width=430,height=420,location=no,directories=no,scrollbars=yes");
+		return false;
+	}
